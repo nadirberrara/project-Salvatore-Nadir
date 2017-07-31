3a164fc0244f33afd140854c9e99ca5c6187f8b8
Minato <nadir.berrara@gmail.com>
2017-07-31 17:05:27 +0200

    created login page

--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,8 @@ router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
 
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
 module.exports = router;
