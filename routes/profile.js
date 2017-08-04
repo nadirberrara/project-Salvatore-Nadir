475cc21c1f84d1e4d4653068ed72740a8e3971bf
Salvatore Dziekan <sdziekan@gmail.com>
2017-08-04 14:21:18 +0200

    dynamic profile url

--- routes/profile.js
+++ routes/profile.js
@@ -9,7 +9,7 @@ router.get("/:name", ensureLoggedIn("/login"), (req, res, next) => {
   res.render("profile", { name: req.user.name});
 });
 
-router.get("/${<%= user.name %>} ", (req, res, next) => {
+router.get("/redirect ", (req, res, next) => {
   res.render("profile/:name", { name: req.user.name});
 });
 
